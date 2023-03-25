86a273a126b6aa079826cf98acb671da85348ccc
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-03-25 15:34:09 +0100

    Cooking slow but steady

--- frontend/src/Views/Home.js
+++ frontend/src/Views/Home.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { HiMenu } from 'react-icons/hi';
 import { AiFillCloseCircle } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 
 import { Sidebar, UserProfile } from "../Components/Index"
 
--- frontend/src/Views/Index.js
+++ frontend/src/Views/Index.js
@@ -1,4 +1,5 @@
 import Home from "./Home";
 import Login from "./Login";
+import Pins from "./Pins";
 
-export { Home, Login }
+export { Home, Login, Pins }
