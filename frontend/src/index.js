bf97fe8a8581f81aabeef0080100f6085d913eee
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-03-25 02:05:04 +0100

    Tired but still cooking

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './Assets/Styles/Index.css';
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from './Contexts/ThemeContext'
+import Background from "./Theme/Background"
 import App from './App';
+import './Assets/Styles/Index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Background>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </Background>
+    </BrowserRouter>
   </React.StrictMode>
 );
