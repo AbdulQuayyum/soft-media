139f305c4879cf4b645f1bf127640a4bc9056bd5
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-03-25 15:29:51 +0100

    Cooking slow but steady

--- frontend/src/Routes/Pins.Routes.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PinsRoutes = () => {
-  return (
-    <div>Pins.Routes</div>
-  )
-}
-
-export default PinsRoutes
--- frontend/src/Views/Home.js
+++ frontend/src/Views/Home.js
@@ -3,6 +3,8 @@ import { HiMenu } from 'react-icons/hi';
 import { AiFillCloseCircle } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 
+import { Sidebar, UserProfile } from "../Components/Index"
+
 const Home = () => {
   return (
     <div>Home</div>
